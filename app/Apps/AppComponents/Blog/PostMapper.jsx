edc1db5b0237080ebbe2aa7fe735efc97ce40849
Presi300 <canaryisle1@hotmail.com>
2024-06-06 23:25:13 +0300

    Stuff

--- app/Apps/AppComponents/Blog/PostMapper.jsx
+++ app/Apps/AppComponents/Blog/PostMapper.jsx
@@ -5,9 +5,9 @@ import { join } from "path";
 import { SanityClient } from "@/app/Sanity/client";
 // Thanks https://github.com/ImRayy for this :)
 
-const path = join(process.cwd(), "/blog/posts");
+const path = join(process.cwd(), "/public/blog/posts");
 
-const imgPath = join(process.cwd() + "/blog/postImages");
+const imgPath = join(process.cwd() + "/public/blog/postImages");
 
 export default async function PostMapper() {
   const files = readdirSync(path);
