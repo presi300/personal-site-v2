3f8d5cadd341adc3eadf0e97a4179ae1fe50b591
Presi300 <canaryisle1@hotmail.com>
2024-07-12 22:04:42 +0300

    updated metadata... yet again

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -6,47 +6,49 @@ import Metadata from "@/components/Metadata";
 
 export default function Page() {
   const router = useRouter();
-  const post = TempPosts.map((e) => e.ShortTitle).indexOf(router.query.slug);
+  const slug = router.query.slug;
+  //   For some unexplained reason, router.query.slug returns undefined and then the actual slug, whenever it's called... So I have to check if slug actually exists before doing anything with it
+  if (slug) {
+    const post = TempPosts.findIndex((e) => e.ShortTitle === slug);
 
-  //For some unexplained reason, the post function returns -1 and then the actual index so I have to do this ridiculous check... what the fuck
-
-  return (
-    <>
-      <Metadata
-        url={`${
-          TempPosts[post]
-            ? `https://presi300.com/blog/${TempPosts[post].ShortTitle} `
-            : "https://presi300.com/blog"
-        }`}
-        title={TempPosts[post] ? TempPosts[post].Title : "Post not found :/"}
-        description="TODO: Add post descriptions"
-        ogImage={
-          TempPosts[post]
-            ? `/blog/postImages/${TempPosts[post].FrontImage}`
-            : ""
-        }
-      ></Metadata>
-      {(typeof window !== undefined && post !== -1 && TempPosts[post] && (
-        <div className="w-screen h-screen pt-[50px] overflow-y-hidden">
-          <BlogOSD Title={TempPosts[post].Title} postID={post}></BlogOSD>
-          <iframe
-            className="w-screen h-full"
-            src={TempPosts[post].Link}
-          ></iframe>
-        </div>
-      )) || (
-        <div className="w-screen h-screen flex flex-col items-center justify-center">
-          <h1>{`Oops... Post "${router.query.slug}" was not found.`}</h1>
-          <div className="flex flex-col sm:flex-row gap-3">
-            <Button>
-              Return to the <b>blog</b> page?
-            </Button>
-            <Button>
-              Return to the <b>main</b> page?
-            </Button>
+    return (
+      <>
+        <Metadata
+          url={`${
+            TempPosts[post]
+              ? `https://presi300.com/blog/${TempPosts[post].ShortTitle} `
+              : "https://presi300.com/blog"
+          }`}
+          title={TempPosts[post] ? TempPosts[post].Title : "Post not found :/"}
+          description="TODO: Add post descriptions"
+          ogImage={
+            TempPosts[post]
+              ? `/blog/postImages/${TempPosts[post].FrontImage}`
+              : ""
+          }
+        ></Metadata>
+        {(typeof window !== undefined && TempPosts[post] && (
+          <div className="w-screen h-screen pt-[50px] overflow-y-hidden">
+            <BlogOSD Title={TempPosts[post].Title} postID={post}></BlogOSD>
+            <iframe
+              className="w-screen h-full"
+              src={TempPosts[post].Link}
+            ></iframe>
           </div>
-        </div>
-      )}
-    </>
-  );
+        )) || (
+          <div className="w-screen h-screen flex flex-col items-center justify-center">
+            <h1>{`Oops... Post "${router.query.slug}" was not found.`}</h1>
+            <div className="flex flex-col sm:flex-row gap-3">
+              <Button>
+                Return to the <b>blog</b> page?
+              </Button>
+              <Button>
+                Return to the <b>main</b> page?
+              </Button>
+            </div>
+          </div>
+        )}
+      </>
+    );
+  }
 }
